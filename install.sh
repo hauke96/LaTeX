69f0acb68110f78318f40e394a92259033b827a3
Hauke Stieler <mail@hauke-stieler.de>
2016-06-04 19:03:02 +0200

    Added install script and changed the import path of the makrocol package.

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+path=/usr/share/texlive/texmf-dist/tex/generic/hauke96/
+whoami="$(whoami)"
+
+if [ "$whoami" != "root" ]; then
+    echo "This script must be executed as root!"
+    exit 1
+fi
+
+echo "DOWNLOADING NEWEST PACKAGES..."
+echo ""
+echo -n "CREATE $path..."
+sudo mkdir -p $path
+echo "finished"
+echo -n "INSTALL dadp..."
+sudo copy dadp $path
+echo "finished"
+echo -n "INSTALL gail..."
+sudo copy gail $path
+echo "finished"
+echo -n "INSTALL makrocol..."
+sudo copy makrocol $path
+echo "finished"
+echo -n "UPDATE the lr-R file..."
+sudo texhash --quiet
+echo "finished"
+echo ""
+echo "DONE"
+echo "(does not mean that everything just worked fine :P )"
+echo ""
+echo "You can also use \"git clone https://github.com/hauke96/LaTeX.git\" to get get everything.\n\n"
