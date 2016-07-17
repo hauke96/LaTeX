61d389ef01c91a86ecad55efc9abacc85ddec51a
Hauke Stieler <mail@hauke-stieler.de>
2016-07-17 17:28:50 +0200

    Fixed problem with the \normalfont in the dadp and edited the install script.

--- install.sh
+++ install.sh
@@ -2,30 +2,25 @@
 path=/usr/share/texlive/texmf-dist/tex/generic/hauke96/
 whoami="$(whoami)"
 
-if [ "$whoami" != "root" ]; then
-    echo "This script must be executed as root!"
-    exit 1
-fi
-
 echo "DOWNLOADING NEWEST PACKAGES..."
 echo ""
-echo -n "CREATE $path..."
+echo -n "CREATE  folder ...... "
 sudo mkdir -p $path
 echo "finished"
-echo -n "INSTALL dadp..."
+echo -n "INSTALL dadp ........ "
 sudo cp dadp.sty $path
 echo "finished"
-echo -n "INSTALL gail..."
+echo -n "INSTALL gail ........ "
 sudo cp gail.sty $path
 echo "finished"
-echo -n "INSTALL makrocol..."
+echo -n "INSTALL makrocol .... "
 sudo cp makrocol.sty $path
 echo "finished"
-echo -n "UPDATE the lr-R file..."
+echo -n "UPDATE  lr-R file ... "
 sudo texhash --quiet
 echo "finished"
 echo ""
 echo "DONE"
 echo "(does not mean that everything just worked fine :P )"
 echo ""
-echo "You can also use \"git clone https://github.com/hauke96/LaTeX.git\" to get get everything.\n\n"
+echo "You can also use \"git clone https://github.com/hauke96/LaTeX.git\" to get get everything."
